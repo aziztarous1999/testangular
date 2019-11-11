be52a9d8774f3c90bc7a27dbee347b2ec8799075
azza <zaghouani.yosri@gmail.com>
2019-11-11 08:50:19 +0100

    commencer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FilmsListComponent } from './films-list/films-list.component';
+import { FilmComponent } from './film/film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsListComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule
